f5aeb192541fdd7fa3f679abb3f19f684afe9f24
MohammedAref11 <mohammedaref.business11@gmail.com>
2026-06-29 12:57:02 +0400

    finished hero section and made it responsive

--- main/main.js
+++ main/main.js
@@ -1,8 +1,9 @@
 const hamBtn = document.getElementById('ham-btn');
-
+const mainNav = document.getElementById("main-nav")
 hamBtn.addEventListener("click", (e) => {
     const el = e.currentTarget; 
     el.classList.toggle("clicked");
     el.classList.toggle("gap-2"); 
+    mainNav.classList.toggle("clicked")
 })
 
